0a2d7133c15850060ad4509f504518bcf71b4561
KanKK <286454796@qq.com>
2019-04-12 15:10:32 +0800

    添加types

--- types/performance.d.ts
+++ types/performance.d.ts
@@ -1,5 +1,15 @@
 export declare class Performance {
   constructor (options?: PerformanceOption);
+
+  private options: PerformanceOption;
+  private performance: any
+  private report: string
+
+  init(): void;
+  getReportText(): string;
+  getWhitePageTime(): number;
+  
+  private displayTiming(): void;
 }
 
 export interface PerformanceOption {
