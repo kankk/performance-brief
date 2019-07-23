f4e94abdc83cccc3ba0fd66ccd3ac375cd5ee17d
KanKK <286454796@qq.com>
2019-07-23 10:44:39 +0800

    fx: 校验输入的版本号是否有效

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -1,5 +1,6 @@
-const _package = require('../package.json');
+const currentPackage = require('../package.json');
 const chalk = require('chalk');
+const semver = require('semver');
 
 const fs = require('fs');
 const path = require('path');
@@ -11,12 +12,21 @@ function run() {
   }
   
   const version = process.argv[2];
+  const currentVersion = currentPackage.version;
+
+  // 判断输入的版本号是否合法
+  if (!semver.valid(version)) {
+    console.log(chalk.red('请输入有效版本'));
+    return;
+  } else if (!semver.gt(version, currentVersion)) {
+    console.log(chalk.red('新的版本需要大于旧的版本'));
+  }
 
   // 根据输入的version修改package.json的version
-  _package.version = version;
+  currentPackage.version = version;
 
   // 覆盖原package.json
-  fs.writeFileSync(path.resolve(process.cwd(), './package.json'), JSON.stringify(_package, null, 2));
+  fs.writeFileSync(path.resolve(process.cwd(), './package.json'), JSON.stringify(currentPackage, null, 2));
 }
 
 run();
